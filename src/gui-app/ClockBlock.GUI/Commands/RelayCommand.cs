9908dbddac88dfdde55520752a1d44ca0ac9d451
Jesse Watson <jexxe@live.com>
2024-10-25 13:58:34 -0400

    Add null safety handling and refactor RelayCommand

---
+++ src/gui-app/ClockBlock.GUI/Commands/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ClockBlock.GUI.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+
+        public void Execute(object? parameter)
+        {
+            if (CanExecute(parameter)) // Check before executing
+            {
+                _execute();
+            }
+        }
+
+        // Initialize the event to an empty delegate to avoid null checks
+        public event EventHandler? CanExecuteChanged = delegate { };
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
